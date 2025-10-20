43dcd7b5d877de55b60a711e1c2e5dc29a5bae32
Zack Alvey <zack.alvey@gmail.com>
2025-10-20 10:37:58 -0600

    started updating links to lessons

--- js/index.js
+++ js/index.js
@@ -3,95 +3,11 @@ const baseUrl = 'http://127.0.0.1:5501/';
 const localUrl = '/';
 const menu = {
   resources: [
-    { url: 'resources/syllabus.html', name: 'Syllabus' },
-    { url: 'resources/dryoarimages.html', name: 'DryOar Images' },
-    { url: 'resources/subpage_content.html', name: 'SubPage Content' },
-    { url: 'resources/design-principles.html', name: 'Design Principles' },
-    { url: 'resources/troubleshooting.html', name: 'Troubleshooting' },
-    { url: 'resources/optimizing-images.html', name: 'Image Optimization' }
+    { url: 'resources/syllabus.html', name: 'Syllabus' }
   ],
   activites: [
-    { url: 'activities/software-setup.html', name: 'Software Setup', week: 1 },
-    { url: 'activities/aboutme.html', name: 'About Me/HTML Vocab', week: 1 },
-    { url: 'activities/site-planning.html', name: 'Site Planning', week: 2 },
-    { url: 'activities/cssvocab.html', name: 'CSS Vocab', week: 2 },
-    {
-      url: 'activities/1-getting-started.html',
-      name: 'Start the home page',
-      week: 3
-    },
-    { url: 'activities/2-adding-css.html', name: 'Adding CSS', week: 4 },
-    { url: 'activities/3-more-css.html', name: 'More CSS', week: 5 },
-    { url: 'activities/elevator-pitch.html', name: 'Elevator Pitch', week: 5 },
-    {
-      url: 'activities/css-positioning.html',
-      name: 'CSS Positioning',
-      week: 6
-    },
-    {
-      url: 'resources/layout-resources.html',
-      name: 'CSS Layout Reading',
-      week: 6
-    },
-
-    {
-      url: 'activities/4-final-touches.html',
-      name: 'Finishing Touches',
-      week: 7
-    },
-    {
-      url: 'activities/personal-site-plan.html',
-      name: 'Personal Site PLan',
-      week: 7
-    },
-    {
-      url: 'activities/your-own-wireframe.html',
-      name: 'Wireframing',
-      week: 8
-    },
-    {
-      url: 'activities/personal-site-content.html',
-      name: 'Personal Site Plan 2: Content',
-      week: 8
-    },
-    { url: 'activities/subpage1.html', name: 'Making a Subpage', week: 8 },
-    { url: 'activities/final.html', name: 'DryOar Final', week: 9 },
-    {
-      url: 'activities/person-site-plan-complete.html',
-      name: 'Completed Personal Site Plan',
-      week: 9
-    },
-    { url: 'activities/forms.html', name: 'HTML Forms', week: 10 },
-    {
-      url: 'activities/personal-site-html.html',
-      name: 'Personal Site HTML',
-      week: 10
-    },
-    {
-      url: 'activities/css-positioning-2.html',
-      name: 'Layour Revisited',
-      week: 11
-    },
-    {
-      url: 'activities/personal-site-home.html',
-      name: 'Personal Site Home',
-      week: 11
-    },
-    {
-      url: 'activities/card.html',
-      name: 'Creating a Media Card component',
-      week: 12
-    },
-    {
-      url: 'activities/personal-site-sub1.html',
-      name: 'Personal Site Page Two',
-      week: 12
-    },
-    {
-      url: 'activities/final.html',
-      name: 'Final',
-      week: 13
-    }
+    { url: 'activities/week01/software-setup.html', name: 'Software Setup', week: 1 },
+    { url: 'activities/week01/folder-setup.html', name: 'Folder Setup', week: 1 }
   ]
 };
 
